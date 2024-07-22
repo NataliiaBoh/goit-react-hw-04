aa90884884c155e245ea253ea6e97fc6d46c410e
NataliiaBoh <homesweethome160126@gmail.com>
2024-07-22 22:06:14 +0300

    add components

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,31 @@
+// import { BsSearch } from "react-icons/bs";
+// import toast, { Toaster } from "react-hot-toast";
+
+export default function searchBar({ onSearch }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.searchQuery.value;
+    query.trim() === ``
+      ? toast.error(`Input can not be empty!`)
+      : onSearch(query);
+    event.target.reset();
+  };
+  return (
+    <header>
+      {/* <Toaster /> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="searchName"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+        {/* <button type="submit">
+          <BsSearch />
+        </button> */}
+      </form>
+    </header>
+  );
+}
